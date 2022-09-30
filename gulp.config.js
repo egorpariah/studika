da2e8b3e35f1013eb8c8c5f34777b52319a29fdc
Egor Golikov <elai.asti@gmail.com>
2022-10-01 05:55:08 +0700

    Update 2022-09-30T22:55:05.536Z

--- gulp.config.js
+++
@@ -1,6 +0,0 @@
-export const SRC_PATH = "src";
-export const DIST_PATH = "dist";
-export const STYLES_LIBS = [
-  "node_modules/normalize.css/normalize.css",
-  "node_modules/simplebar/dist/simplebar.css",
-];
